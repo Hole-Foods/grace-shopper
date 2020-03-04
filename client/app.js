b97825a005c2882ce229ba3324617115fa60d922
Brian Chung <brianchung@live.com>
2020-03-04 08:34:55 -0600

    adds bootstrap + styled components

--- client/app.js
+++ client/app.js
@@ -1,15 +1,16 @@
-import React from 'react'
-
-import {Navbar} from './components'
-import Routes from './routes'
+import React from 'react';
+import GlobalStyle from './styles/Global';
+import { Navbar } from './components';
+import Routes from './routes';
 
 const App = () => {
   return (
     <div>
+      <GlobalStyle />
       <Navbar />
       <Routes />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/styles/Global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+body {
+  color: #000000;
+  background-color: #FFFFFF;
+  margin: 0;
+  font-family: '';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+`;
+
+export default GlobalStyles;
