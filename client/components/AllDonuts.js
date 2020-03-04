c8477d61dae699921f155b0e62d484b188e27115
Drew Lu <drewylu@gmail.com>
2020-03-04 11:12:12 -0600

    AllDonuts route/dux complete

---
+++ client/components/AllDonuts.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const AllDonuts = props => {
+  const donuts = props.donuts;
+
+  return (
+    <div>
+      {donuts.map(donut => (
+        <div key={donut.id} className="donut row">
+          <Link to={`/donuts/${donut.id}`}>
+            <img src={donut.imageUrl} />
+            <p>{donut.name}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    donuts: state.donuts,
+    donut: state.donut,
+  };
+};
+
+export default connect(mapStateToProps)(AllDonuts);
