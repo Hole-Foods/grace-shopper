2563c00e187bed87109cbe746b94b7c8056e6ead
Brian Chung <brianchung@live.com>
2020-03-06 11:01:41 -0600

    quick adjustment to add cart item to default user

--- script/seed.js
+++ script/seed.js
@@ -1,12 +1,19 @@
 'use strict';
 const chance = require('chance')(123);
 const db = require('../server/db');
-const { User, Review, Donut, Category } = require('../server/db/models');
+const {
+  User,
+  Review,
+  Donut,
+  Category,
+  CartItem,
+} = require('../server/db/models');
 
 const NUM_USERS = 5;
 const NUM_CATEGORIES = 5;
 const NUM_DONUTS = 100;
 const NUM_REVIEWS = 100;
+const NUM_CART_ITEMS = 5; // FOR USER EMAIL@EMAIL.COM
 
 const emails = chance.unique(chance.email, NUM_USERS);
 
@@ -49,6 +56,7 @@ const randomDonut = () => {
     name: randomName(),
     description: chance.paragraph({ sentences: 10 }),
     price: chance.floating({ min: 0, max: 100, fixed: 2 }),
+    qty: 100,
     categoryId: chance.integer({ min: 1, max: `${NUM_CATEGORIES}` }),
   });
 };
@@ -59,7 +67,15 @@ const randomReview = () => {
     content: chance.paragraph({ sentences: 10 }),
     rating: chance.integer({ min: 1, max: 5 }),
     userId: chance.integer({ min: 1, max: `${NUM_USERS}` }),
-    donutId: chance.integer({ min: 1, max: 100 }),
+    donutId: chance.integer({ min: 1, max: `${NUM_DONUTS}` }),
+  });
+};
+
+const randomCartItem = () => {
+  return CartItem.build({
+    qty: chance.integer({ min: 1, max: 5 }),
+    userId: 6,
+    donutId: chance.integer({ min: 1, max: `${NUM_DONUTS}` }),
   });
 };
 
@@ -67,6 +83,7 @@ const users = doTimes(NUM_USERS, randomUser);
 const categories = doTimes(NUM_CATEGORIES, randomCategory);
 const donuts = doTimes(NUM_DONUTS, randomDonut);
 const reviews = doTimes(NUM_REVIEWS, randomReview);
+const cartItems = doTimes(NUM_CART_ITEMS, randomCartItem);
 
 const seed = async () => {
   try {
@@ -87,64 +104,13 @@ const seed = async () => {
     console.log(`${donuts.length} donuts seeded!`);
     await Promise.all(reviews.map(review => review.save()));
     console.log(`${reviews.length} reviews seeded!`);
+    await Promise.all(cartItems.map(item => item.save()));
+    console.log(`${cartItems.length} cart items for email@email.com seeded!`);
+    console.log(`everything seeded successfully!`);
   } catch (err) {
     console.log(err);
   }
 };
-
-// async function seed() {
-//   await db.sync({ force: true });
-//   console.log('db synced!');
-
-//   // USERS
-//   const seedUsers = await Promise.all(users.map(usr => User.create(usr)));
-//   console.log(`${seedUsers.length} users seeded!`);
-
-//   // CATEGORIES
-//   await Category.create({ name: 'ring' });
-//   console.log(`ring category created!`);
-//   await Category.create({ name: 'hole' });
-//   console.log(`hole category created!`);
-//   await Category.create({ name: 'bar' });
-//   console.log(`bar category created!`);
-
-//   // DONUTS
-//   const seedDonuts = await Promise.all(
-//     donuts.map(async donut => {
-//       const newDonut = await Donut.create(donut);
-//       const category = await Category.findOne({
-//         where: { id: donut.categoryId },
-//       });
-//       await category.addDonut(newDonut);
-//     })
-//   );
-//   console.log(`${seedDonuts.length} donuts seeded!`);
-
-//   // REVIEWS
-//   const seedReviews = await Promise.all(
-//     reviews.map(async review => {
-//       const newReview = await Review.create(review);
-//       const donut = await Donut.findOne({
-//         where: { id: review.donutId },
-//       });
-//       // await newReview.setReview(donut)
-//       await donut.addReview(newReview);
-//     })
-//   );
-//   console.log(`${seedReviews.length} reviews seeded!`);
-
-//   // CART
-//   const seedCart = await Promise.all(cItems.map(item => CartItem.create(item)));
-//   console.log(`${seedCart.length} items seeded!`);
-
-//   // const users = await Promise.all([
-//   //   User.create({email: 'cody@email.com', password: '123'}),
-//   //   User.create({email: 'murphy@email.com', password: '123'})
-//   // ])
-
-//   // console.log(`seeded ${users.length} users`)
-//   // console.log(`seeded successfully`)
-// }
 
 // We've separated the `seed` function from the `runSeed` function.
 // This way we can isolate the error handling and exit trapping.
