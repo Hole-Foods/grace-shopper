f6aa6c0a95681b81fa9fe96acaa7e604e7fe6052
Brian Chung <brianchung@live.com>
2020-03-07 20:59:20 -0600

    update for full checkout

--- client/routes.js
+++ client/routes.js
@@ -8,7 +8,8 @@ import {
   UserHome,
   AllDonuts,
   Cart,
-  BillShip,
+  OrderCheckout,
+  OrderConfirmation,
   SingleDonut,
 } from './components';
 import { me } from './store';
@@ -30,7 +31,8 @@ class Routes extends Component {
         {/* <Route path="/donut/:donutId" component={SingleDonut} /> */}
         <Route path="/donuts/:donutId" component={SingleDonut} />
         <Route path="/cart" component={Cart} />
-        <Route path="/billship" component={BillShip} />
+        <Route path="/checkout" component={OrderCheckout} />
+        <Route path="/confirmation" component={OrderConfirmation} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/all" component={AllDonuts} />
