fa43bb1d3b5f165858db5e03196dd22971a97a38
Brian Chung <brianchung@live.com>
2020-03-09 09:52:45 -0500

    checkout transaction started

--- script/seed.js
+++ script/seed.js
@@ -11,7 +11,8 @@ const {
 
 const NUM_USERS = 5;
 const NUM_CATEGORIES = 5;
-const NUM_DONUTS = 100;
+const NUM_DONUTS = 10;
+const NUM_QTY = 10;
 const NUM_REVIEWS = 10;
 const NUM_CART_ITEMS = 5; // FOR USER EMAIL@EMAIL.COM
 
@@ -56,7 +57,7 @@ const randomDonut = () => {
     name: randomName(),
     description: chance.paragraph({ sentences: 10 }),
     price: chance.floating({ min: 0, max: 100, fixed: 2 }),
-    qty: `${NUM_DONUTS}`,
+    qty: `${NUM_QTY}`,
     categoryId: chance.integer({ min: 1, max: `${NUM_CATEGORIES}` }),
   });
 };
--- server/api/orders.js
+++ server/api/orders.js
@@ -1,94 +1,89 @@
 const router = require('express').Router();
 const db = require('../db');
-const { User, Address, Order, OrderItem, Donut } = require('../db/models');
+const {
+  User,
+  Address,
+  Order,
+  OrderItem,
+  Donut,
+  CartItem,
+} = require('../db/models');
 const { isLoggedIn } = require('../utils');
 module.exports = router;
 
-const checkStock = cart => {
-  return cart.filter(item => {
-    return item.qty > item.donut.qty;
-  });
-};
-
 const adjustStock = async (id, qty) => {
   const donut = await Donut.findByPk(id);
+  // if (donut.qty < qty) {
+  //   //throw new Error();
+  // }
   donut.qty -= qty;
   await donut.save();
 };
 
 router.put('/', isLoggedIn, async (req, res, next) => {
   try {
-    const result = await db.transaction(async t => {});
-
-    const [address, wasCreated] = await Address.findOrCreate({
-      where: { address1: req.body.address1, address2: req.body.address2 },
-      defaults: {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        address2: req.body.address2,
-        city: req.body.city,
-        state: req.body.state,
-        zip: req.body.zip,
-        country: req.body.country,
-      },
-    });
-
-    const user = await User.findByPk(req.user.id);
-    //console.log(Object.keys(user.__proto__));
-    const cartItems = await user.getCartItems({
-      include: [
-        {
-          model: Donut,
-          attributes: ['name', 'imageUrl', 'qty', 'price'],
+    const result = await db.transaction(async t => {
+      // transaction start
+      const [address] = await Address.findOrCreate({
+        where: { address1: req.body.address1, address2: req.body.address2 },
+        defaults: {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          address2: req.body.address2,
+          city: req.body.city,
+          state: req.body.state,
+          zip: req.body.zip,
+          country: req.body.country,
         },
-      ],
+      });
+
+      const user = await User.findByPk(req.user.id);
+
+      //console.log(Object.keys(user.__proto__));
+
+      const cartItems = await user.getCartItems({
+        include: [
+          {
+            model: Donut,
+            attributes: ['name', 'imageUrl', 'qty', 'price'],
+          },
+        ],
+      });
+      // add check if there are cart items
+      //console.log(Object.keys(order.__proto__));
+
+      const order = await Order.create();
+      await order.setUser(user);
+      await order.setAddress(address);
+
+      const orderItems = await Promise.all(
+        cartItems.map(async item => {
+          await adjustStock(item.donutId, item.qty); // substract from donut stock
+          return OrderItem.create({
+            price: item.donut.price,
+            qty: item.qty,
+            donutId: item.donutId,
+          });
+        })
+      );
+
+      await order.setOrderItems(orderItems); // take in an array of order items
+
+      //await CartItem.destroy({ where: { userId: user.id } }); // finally destroy cart items
+
+      const items = await order.getOrderItems({
+        include: [
+          {
+            model: Donut,
+            attributes: ['name', 'imageUrl', 'qty', 'price'],
+          },
+        ],
+      });
+
+      return { order, items, address };
     });
-
-    //cartItems[0].qty = 11111;
-    // IMPORTANT: NEED TO ADD CHECK IF CART COMES BACK EMPTY
-
-    const notInStock = checkStock(cartItems);
-
-    if (notInStock.length) {
-      res.send('NOT IN STOCK');
-      //console.log('ITEMS NOT IN STOCK', notInStock); // RETURN ITEMS NOT IN STOCK FOR ERROR HANDLING
-    }
-
-    const order = await Order.create();
-    await order.setUser(user);
-    await order.setAddress(address);
-
-    //console.log(Object.keys(order.__proto__));
-
-    const orderItems = await Promise.all(
-      cartItems.map(async item => {
-        await adjustStock(item.donutId, item.qty); // substract from donut stock
-        return OrderItem.create({
-          price: item.donut.price,
-          qty: item.qty,
-          donutId: item.donutId,
-        });
-      })
-    );
-
-    await order.setOrderItems(orderItems); // take in an array of order items
-
-    //await CartItem.destroy({ where: { userId: user.id } }); // finally destroy cart items
-
-    const items = await order.getOrderItems({
-      include: [
-        {
-          model: Donut,
-          attributes: ['name', 'imageUrl', 'qty', 'price'],
-        },
-      ],
-    });
-
-    res.json({
-      order,
-      items,
-      address,
-    });
+    // transaction commits here
+    res.json(result);
   } catch (err) {
     next(err);
   }
